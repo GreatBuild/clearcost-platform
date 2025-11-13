875dd92e2e2efa639eae10cd072bdb61ad8a98fb
CodyLionVivo <leon.fabri.vivas@hotmail.com>
2025-11-12 20:01:48 -0500

    Add Docker orchestration for microservices stack

---
+++ docker/mysql/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS clearcost_users_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS clearcost_orgs_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS clearcost_invitations_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS clearcost_projects_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS clearcost_change_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
